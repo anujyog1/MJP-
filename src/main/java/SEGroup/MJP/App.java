4d3ffd17669bbc73963b69c08b280e30fe253988
Yogita <agyogita@gmail.com>
2023-10-12 16:20:56 +0530

    committing MJP files

---
+++ src/main/java/SEGroup/MJP/App.java
@@ -0,0 +1,13 @@
+package SEGroup.MJP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello AI&ML C section students How is SE lab" );
+    }
+}
